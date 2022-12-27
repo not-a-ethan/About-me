0cbf21f4d1623156ed8c43893f38adc27999ad37
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2022-12-27 03:44:57 +0000

    Made a lot of progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Skills = () => {
+  const mostStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "1vw",
+    textAlign: "center",
+  }
+
+  const itemStyle = {
+    color: "white",
+    background: "#0108B3",
+    border: "1px solid #000000",
+    borderRadius: "100px",
+    minWidth: "5%",
+    maxWidth: "7.5%",
+    height: "1.775vh",
+    gridColumn: "1",
+    gridRow: "1",
+  }
+
+  const content = (
+    <div>
+      <h1>Skills</h1>
+      <div style={mostStyle}>
+        <p style={itemStyle}>HTML</p>
+        <p style={itemStyle}>CSS</p>
+        <p style={itemStyle}>JavaScript</p>
+        <p style={itemStyle}>Github</p>
+      </div>
+    </div>
+  )
+
+  return content;
+}
+
+const App = () => {
+  const allStyle = {
+  // I want a light grey backtground rgb
+    background: "rgb(160, 160, 160)",
+    width: "100vw",
+    height: "100vh",
+    padding: "0",
+    margin: "0",
+  }
+
+  const mostStyle = {
+    marginLeft: "2vw",
+    marginTop: "2vh",
+  }
+
+  const pfpStyle = {
+    "borderRadius": "100%",
+  }
+
+  const content = (
+    <div style={allStyle}>
+      <br />
+      <div style={mostStyle}>
+        <img src="./images/pfp.png" alt="pfp" style={pfpStyle} />
+        <h2>Ethan</h2>
+
+        <Skills />
+      </div>
+    </div>
+  )
+
+  return content;
+}
+
+root.render(<App />);
